3b58e5887f885257d3b83b1f0f3438dd93a2d732
minsoo.lee <letomok@gmail.com>
2021-12-26 00:40:49 +0900

    scaffold

--- rich/lib/main.dart
+++ rich/lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 // The main function is the starting point for all our Flutter apps.
 void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey[600],
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage("https://media.istockphoto.com/photos/lion-sitting-looking-away-panthera-leo-10-years-old-isolated-picture-id455663609?k=20&m=455663609&s=612x612&w=0&h=1J2LDKEn05YnOIm0XmZ69VUdH39KUGWfiBF1bEouNMo="),
+            ),
+          ),
         ),
       ),
     );
