a64a5817a18007f068f4d0f35460c85bbf28d7b9
minsoo.lee <letomok@gmail.com>
2022-01-06 00:43:51 +0900

    assets

--- rich/lib/main.dart
+++ rich/lib/main.dart
@@ -11,7 +11,7 @@ void main() => runApp(
           ),
           body: Center(
             child: Image(
-              image: NetworkImage("https://media.istockphoto.com/photos/lion-sitting-looking-away-panthera-leo-10-years-old-isolated-picture-id455663609?k=20&m=455663609&s=612x612&w=0&h=1J2LDKEn05YnOIm0XmZ69VUdH39KUGWfiBF1bEouNMo="),
+              image: AssetImage('images/cat.jpeg'),
             ),
           ),
         ),
